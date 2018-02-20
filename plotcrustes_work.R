3f74bfd6b80f6334908017de3790a837b04eb1ca
Robin Shields-Cutler <cutler.robin@gmail.com>
2018-02-20 11:52:18 -0600

    Updated permutation scheme and added comments throughout

--- plotcrustes_work.R
+++ plotcrustes_work.R
@@ -7,72 +7,85 @@ library(vegan)
 library(ggplot2)
 
 # Read in beta diversity tables
-unw_A <- read.delim('../unweighted_unifrac_stool_pre_ampOTUnorm.txt',
+unw_A <- read.delim('../unweighted_unifrac_asp_post_ampOTUnorm.txt',
                       header=1, row.names = 1, check.names = F)
 unw_B <- read.delim('../unweighted_unifrac_stool_post_ampOTUnorm.txt',
                        header=1, row.names = 1, check.names = F)
-meta <- read.delim('../plotcrust_metadata.txt',
+# Metadata needs three columns - 
+# 1. sample IDs (e.g. "P21_pre_stool")
+# 2. the unifying participant/unit ID (e.g."P21") 
+# 3. the binary metadata group (e.g. "pre")
+meta <- read.delim('../plotcrust_metadata_aspstool.txt',
                    header=1, row.names=1, check.names = F)
 
 thing <- 'patient'
 group <- 'group'
-groups <- c('pre','post')
-metaA <- meta[meta[,group] == groups[1],]
+groups <- c('asp','stool')  # Names of the two distance matrices/categories
+metaA <- meta[meta[,group] == groups[1],]  # Split the metadata into the two groups
 metaB <- meta[meta[,group] == groups[2],]
 
-metaA <- metaA[order(metaA[,thing]),]
+metaA <- metaA[order(metaA[,thing]),]  # Sort the metadata by unifying ID
 metaB <- metaB[order(metaB[,thing]),]
 
+# CRITICAL:
+# Ensure that the original distance matrices are in the same order by participant
+# Uses the order generated from the metadata dataframe
 unw_A <- unw_A[rownames(metaA),rownames(metaA)]
 unw_B <- unw_B[rownames(metaB),rownames(metaB)]
 
+# Get the principal coordinates
 pcoa_A <- pcoa(unw_A)$vectors
 for(c in 1:ncol(pcoa_A)){
   colnames(pcoa_A)[c] <- paste0("PC",c)
 }
-
 pcoa_B <- pcoa(unw_B)$vectors
 for(c in 1:ncol(pcoa_B)){
   colnames(pcoa_B)[c] <- paste0("PC",c)
 }
 
-crusty <- procrustes(pcoa_A, pcoa_B, symmetric = T)
-crust_test_p <- protest(pcoa_A, pcoa_B, permutations = how(nperm = 999))
-A_crust <- data.frame(crusty$X)
-B_crust <- data.frame(crusty$Yrot)
+crusty <- procrustes(pcoa_A, pcoa_B, symmetric = T)  # Run Procrustes
+crust_test_p <- protest(pcoa_A, pcoa_B, permutations = how(nperm = 999))$signif
+A_crust <- data.frame(crusty$X)  # Recover the first group's coordinates
+B_crust <- data.frame(crusty$Yrot)  # Recover the second group's coordinates
 colnames(B_crust) <- colnames(A_crust)
 ncoords = as.numeric(ncol(A_crust))
 A_crust <- merge(A_crust, metaA, by=0)
 B_crust <- merge(B_crust, metaB, by=0)
-rownames(A_crust) <- A_crust[,1]; A_crust[,1] <- NULL
+rownames(A_crust) <- A_crust[,1]; A_crust[,1] <- NULL  # Merge makes rownames column
 rownames(B_crust) <- B_crust[,1]; B_crust[,1] <- NULL
-# B_crust$sample <- lapply(X = rownames(B_crust), FUN = function(xx)
-#   strsplit(x=xx, split = 'endo', fixed = T)[[1]][1])
-sample_ids <- A_crust[,thing]
+sample_ids <- A_crust[,thing]  # Get all the unifying participant IDs
 
-real_dist <- data.frame(matrix(nrow = length(sample_ids), ncol = 3))
+real_dist <- data.frame(matrix(nrow = length(sample_ids), ncol = 3))  # Initialize the dataframe
 colnames(real_dist) <- c('sampleID_A', 'sampleID_B', 'distance')
+# Loop through each participant to get the multidimensional distance between their rotated points
 for (i in 1:length(sample_ids)) {
   ix <- as.character(sample_ids[i])
-  A_ix <- A_crust[A_crust[,thing] == ix, 1:ncoords]
+  A_ix <- A_crust[A_crust[,thing] == ix, 1:ncoords]  # Keep all the PC axes
   B_ix <- B_crust[B_crust[,thing] == ix, 1:ncoords]
   AB_mat <- rbind(A_ix, B_ix)
-  AB_dist <- matrix(dist(AB_mat, method = 'euclidean'))
-  real_dist[i,1] <- as.character(rownames(A_ix))
+  AB_dist <- matrix(dist(AB_mat, method = 'euclidean'))  # Calculates the distance
+  real_dist[i,1] <- as.character(rownames(A_ix))  # Fill in the dataframe
   real_dist[i,2] <- as.character(rownames(B_ix))
   real_dist[i,3] <- as.numeric(AB_dist[1])
 }
 
 A_crust$timepoint <- 'A'; B_crust$timepoint <- 'B'
 A_crust$realperm <- 'real'; B_crust$realperm <- 'real'
-pro_dist <- sqrt(sum(real_dist$distance^2))
+
+# The "Procrustes Distance"
+# pro_dist <- sqrt(sum(real_dist$distance^2))
 
 
-#Permute
+### Single permutation ###
 
 metaBp <- metaB
+# Permute the participant IDs to scramble the pair matching
+# Later, people will be paired with non-self points for distance
+
 metaBp[,thing] <- sample(x = metaBp[,thing], size = length(metaBp[,thing]), replace = F)
 
+
+# Same as above
 crusty_p <- procrustes(pcoa_A, pcoa_B, symmetric = T)
 crust_test_p <- protest(pcoa_A, pcoa_B, permutations = how(nperm = 99))$signif
 Ap_crust <- data.frame(crusty_p$X)
@@ -88,7 +101,6 @@ perm_dist <- data.frame(matrix(nrow = length(sample_ids), ncol = 3))
 colnames(perm_dist) <- c('sampleID_A', 'sampleID_B', 'distance')
 for (i in 1:length(sample_ids)) {
   ix <- as.character(sample_ids[i])
-  # ixp <- as.character(sample_id_perm[i])
   Ap_ix <- Ap_crust[Ap_crust[,thing] == ix, 1:14]
   Bp_ix <- Bp_crust[Bp_crust[,thing] == ix, 1:14]
   ABp_mat <- rbind(Ap_ix, Bp_ix)
@@ -119,16 +131,24 @@ perm_pro_dist <- sqrt(sum(perm_dist$distance^2))
 #   geom_line(aes(x=PC1, y=PC2, group=patient)) +
 #   theme_classic() + facet_grid(. ~ realperm)
 
-# make a bunch
+### Multiple permutations ###
+
+# Repeat the permutation "j" times
 real_dist$real_perm <- 'true_distance'
 dist_plot <- real_dist
-PCOA_plot_many <- rbind(A_crust, B_crust)
-for (j in 1:9) { 
+PCOA_plot_many <- rbind(A_crust, B_crust)  # Start with the real data, then add the permutations
+for (j in 1:4) { 
   metaBp <- metaB
   metaBp[,thing] <- sample(x = metaBp[,thing], size = length(metaBp[,thing]), replace = F)
-  
-  crusty_p <- procrustes(pcoa_A, pcoa_B, symmetric = T)
-  crust_test_perm_p <- protest(pcoa_A, pcoa_B, permutations = how(nperm = 999))$signif
+  metaBp <- metaBp[order(metaBp[,thing]),]
+  unw_Bp <- unw_B[rownames(metaBp),rownames(metaBp)]
+  pcoa_Bp <- pcoa(unw_Bp)$vectors
+  for(c in 1:ncol(pcoa_Bp)){
+    colnames(pcoa_Bp)[c] <- paste0("PC",c)
+  }
+  crusty_p <- procrustes(pcoa_A, pcoa_Bp, symmetric = T)
+  crust_test_perm_p <- protest(pcoa_A, pcoa_Bp, permutations = how(nperm = 999))$signif
+  cat(paste0(crust_test_perm_p, '\n'))
   Ap_crust <- data.frame(crusty_p$X)
   Bp_crust <- data.frame(crusty_p$Yrot)
   colnames(Bp_crust) <- colnames(Ap_crust)
@@ -152,23 +172,30 @@ for (j in 1:9) {
     perm_dist[i,3] <- as.numeric(ABp_dist[1])
   }
   perm_dist_plot_2 <- perm_dist
-  perm_dist_plot_2$real_perm <- paste0('permuted',j)
+  perm_dist_plot_2$real_perm <- paste0('permuted',j)  # Keep track of the permuted data
   dist_plot <- rbind(dist_plot, perm_dist_plot_2)
   
+  # Add the permuted data to the existing runs
   Ap_crust$timepoint <- 'A'; Bp_crust$timepoint <- 'B'
   Ap_crust$realperm <- paste0('permuted',j); Bp_crust$realperm <- paste0('permuted',j)
   PCOA_plot_many <- rbind(PCOA_plot_many, Ap_crust, Bp_crust)
 }
 
+
+### Plot the results ###
+# Boxplot with scatter
 pbox <- ggplot(dist_plot, aes(x=real_perm, y=distance, group=real_perm)) +
   geom_boxplot(outlier.colour = 'white') + geom_jitter(width = 0.2) + theme_classic() +
   theme(axis.text.x = element_text(angle=45, hjust=1), axis.title.x = element_blank(),
         axis.text = element_text(color='black'))
-ggsave(pbox, filename = '../distance_boxplots10x.png', width = 5, height = 3.5, dpi = 300)
+# To save
+ggsave(pbox, filename = '../distance_boxplots_aspstool_10x.png', width = 5, height = 3.5, dpi = 300)
 
+# Set of typical procrustes plots
 pscat <- ggplot(PCOA_plot_many) + geom_point(aes(x=PC1, y=PC2, color=timepoint)) +
   geom_line(aes(x=PC1, y=PC2, group=patient)) +
   theme_classic() + facet_grid(. ~ realperm) +
   theme(axis.text = element_text(color='black', size = 6))
-ggsave(pscat, filename = '../permute10x.png', width = 10, height = 1.5, dpi = 300)
+# To save
+ggsave(pscat, filename = '../permute10x_aspstool.png', width = 10, height = 1.5, dpi = 300)
 
